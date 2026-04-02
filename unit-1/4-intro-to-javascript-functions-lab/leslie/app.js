6249a95180e0e7613228dd7c19014bafa7387cc7
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-02 17:56:59 +0800

    Update app.js with Level Up Exercise 12

--- unit-1/4-intro-to-javascript-functions-lab/leslie/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/leslie/app.js
@@ -254,3 +254,16 @@ const createUsername = (firstname = "", lastname = "") =>
   (firstname.length + lastname.length);
 
 console.log("Exercise 11 Result:", createUsername("Samantha", "Green"));
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+const numArgs = (...args) => args.length;
+
+console.log("Exercise 12 Result:", numArgs(1, 2, 3, 4));
