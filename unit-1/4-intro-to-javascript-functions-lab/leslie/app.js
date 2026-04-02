bea73659255105ea8506f703135ebce4ca7f1c19
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-02 17:43:20 +0800

    Update app.js with Exercise 9. Fix calculation for Exercise 8

--- unit-1/4-intro-to-javascript-functions-lab/leslie/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/leslie/app.js
@@ -153,11 +153,11 @@ const convertTemperature = (temperature, scale) => {
   const result = [];
   switch (scale) {
     case "C":
-      result[0] = Math.round(temperature * 10 * 1.8 + 32) / 10;
+      result[0] = Math.round(temperature * 10 * 1.8) / 10 + 32;
       result[1] = "(Fahrenheit)";
       break;
     case "F":
-      result[0] = Math.round(((temperature * 10 - 32) * 5) / 9) / 10;
+      result[0] = Math.round(((temperature - 32) * 10 * 5) / 9) / 10;
       result[1] = "(Celsius)";
       break;
     default:
@@ -167,3 +167,36 @@ const convertTemperature = (temperature, scale) => {
 };
 
 console.log("Exercise 8 Result:", convertTemperature(32, "C"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+const basicCalculator = (valueA, valueB, operation) => {
+  switch (operation) {
+    case "add":
+      return valueA + valueB;
+    case "subtract":
+      return valueA - valueB;
+    case "multiply":
+      return valueA * valueB;
+    case "divide":
+      return valueA / valueB;
+    default:
+      return "operation is not supported.";
+  }
+};
+
+console.log("Exercise 9 Result:", basicCalculator(10, 5, "subtract"));
