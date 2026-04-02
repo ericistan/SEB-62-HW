7a049b39363c719bbe9d860fbfcdea910676f31f
Lincoln <lklim050@gmail.com>
2026-04-03 00:38:20 +0800

    exer 11 of unit1-4 done

--- unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
@@ -100,28 +100,163 @@
 // console.log("Exercise 5 Result:", greetUser("Sam", "morning"));
 // // Expected result would be Good morning, Sam!
 
-/*
-Exercise 6: maxOfThree()
+// /*
+// Exercise 6: maxOfThree()
 
-Define a function, maxOfThree. It should accept three numbers 
-and return the largest among them.
+// Define a function, maxOfThree. It should accept three numbers
+// and return the largest among them.
 
-Example: maxOfThree(17, 4, 9) should return 17.
+// Example: maxOfThree(17, 4, 9) should return 17.
 
-Complete the exercise in the space below:
-*/
+// Complete the exercise in the space below:
+// */
 
-// Practice declarative function
-function maxOfThree(x, y, z) {
-  if (x >= y && x >= z) {
-    return x;
-  } else if (y >= z) {
-    return y;
-  } else return z;
-}
+// // Practice declarative function
+// function maxOfThree(x, y, z) {
+//   if (x >= y && x >= z) {
+//     return x;
+//   } else if (y >= z) {
+//     return y;
+//   } else return z;
+// }
 
-// Try ternary
-const maxOfThreeT = (x, y, z) => (x >= y && x >= z ? x : y >= z ? y : z);
+// // Try ternary
+// const maxOfThreeT = (x, y, z) => (x >= y && x >= z ? x : y >= z ? y : z);
 
-console.log("Exercise 6 Result:", maxOfThree(5, 10, 8));
-console.log("Exercise 6 Result using ternary:", maxOfThreeT(5, 10, 8));
+// console.log("Exercise 6 Result:", maxOfThree(5, 10, 8));
+// console.log("Exercise 6 Result using ternary:", maxOfThreeT(5, 10, 8));
+// Both expected result to be 10
+
+// /*
+// Exercise 7: calculateTip()
+
+// Create a function called calculateTip. It should take two arguments:
+// the bill amount and the tip percentage (as a whole number).
+// The function should return the amount of the tip.
+
+// Example: calculateTip(50, 20) should return 10.
+
+// Complete the exercise in the space below:
+// */
+// const calculateTip = (bill, tipPercent) => (bill * tipPercent) / 100;
+
+// console.log("Exercise 7 Result:", calculateTip(50, 20));
+// // Expected result to be 10
+
+// /*
+// Exercise 8: convertTemperature()
+
+// Write a function named convertTemperature.
+// It takes two arguments: a temperature and a string representing the
+// scale ('C' for Celsius, 'F' for Fahrenheit).
+// Convert the temperature to the other scale.
+
+// Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+// Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+// Complete the exercise in the space below:
+// */
+// const convertTemperature = (temp, string) =>
+//   string === "C" ? temp : string === "F" ? (temp * 9) / 5 + 32 : undefined;
+// console.log("Exercise 8 Result:", convertTemperature(32, "C"));
+// // Expected result should be 32 as there is no change
+
+// /*
+// Exercise 9: basicCalculator()
+
+// Create a function named basicCalculator.
+// It should take three arguments: two numbers and a string representing
+// an operation ('add', 'subtract', 'multiply', 'divide').
+// Perform the provided operation on the two numbers.
+// In operations where the order of numbers is important,
+// treat the first parameter as the first operand and the
+// second parameter as the second operand.
+
+// Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+// Complete the exercise in the space below:
+// */
+
+// const basicCalculator = (x, y, str) => {
+//   switch (str) {
+//     case "add":
+//       return x + y;
+//       break;
+//     case "subtract":
+//       return x - y;
+//       break;
+//     case "multiply":
+//       return x * y;
+//       break;
+//     case "divide":
+//       return x / y;
+//       break;
+//   }
+// };
+
+// console.log("Exercise 9 Result:", basicCalculator(10, 5, "subtract"));
+
+// /*
+// Exercise 10: calculateGrade()
+
+// Define a function called calculateGrade.
+// It should take a numerical score and return the corresponding letter
+// grade (A, B, C, D, F).
+
+// For example, 90 and above yields an 'A', 80-89 is a 'B',
+// and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+// Example: calculateGrade(100) should return A.
+
+// Complete the exercise in the space below:
+// */
+// const calculateGrade = (grade) => {
+//   if (grade <= 100 && grade >= 90) {
+//     return "A";
+//   } else if (grade < 90 && grade >= 80) {
+//     return "B";
+//   } else if (grade < 80 && grade >= 71) {
+//     return "C";
+//   } else if (grade < 70 && grade >= 61) {
+//     return "D";
+//   } else if (grade < 60 && grade >= 0) {
+//     return "F";
+//   } else return "invalid result";
+// };
+
+// console.log("Exercise 10 Result:", calculateGrade(85));
+
+// /*
+// Exercise 11: createUsername()
+
+// Define a function called createUsername.
+// It should take a first name and a last name and return a username.
+
+// The username should be a combination of the following:
+// - The first three letters of the first name.
+// - The first three letters of the last name.
+// - The total character count of the first and last name combined.
+
+// Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+// Complete the exercise in the space below:
+// */
+
+// createUsername (firstName, lastName) {
+//     const fnArr = [firstName].split("");
+//     fnArr.split("");
+
+// }
+
+// console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+// /*
+// Exercise 12: numArgs()
+
+// Challenge yourself with numArgs.
+// This function should return the count of arguments passed to it when called.
+
+// Complete the exercise in the space below:
+// */
+
+// console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
