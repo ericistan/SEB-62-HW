dfec5bf94b56393a2b0e21ca6a59a8c919135763
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-02 16:45:49 +0800

    Update app.js with Exercise 3

--- unit-1/4-intro-to-javascript-functions-lab/leslie/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/leslie/app.js
@@ -36,3 +36,22 @@ const isAdult = (age) => {
 };
 
 console.log("Exercise 2 Result:", isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = (char) => {
+  return "aeiou".includes(char.toLowerCase());
+};
+
+console.log("Exercise 3 Result:", isCharAVowel("a"));
