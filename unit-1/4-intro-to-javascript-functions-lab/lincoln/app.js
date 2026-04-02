85a88a9071d60191560c9d5d6745957e8c5d69c6
Lincoln <lklim050@gmail.com>
2026-04-02 22:47:57 +0800

    complete exercise 3 of unit 1-4

--- unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
@@ -21,25 +21,46 @@
 // //numbers 3 and 9 passed into x and y respectively of the function maxOfTwoNumbers
 // // result expected is to be 9 since the purpose of the function is to check and return the larger number
 
+// /*
+// Exercise 2: isAdult()
+
+// Write a function named isAdult. It should take an age (number)
+// and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+// Example: isAdult(21) should return 'Adult'.
+
+// Complete the exercise in the space below:
+// */
+
+// const isAdult = (x) => {
+//   const adultAge = 18;
+//   if (x >= adultAge) {
+//     return "Adult";
+//   } else {
+//     return "Minor";
+//   }
+// };
+
+// console.log("Exercise 2 Result:", isAdult(21));
+// // Expect result to be Adult
+
 /*
-Exercise 2: isAdult()
+Exercise 3: isCharAVowel()
 
-Write a function named isAdult. It should take an age (number) 
-and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
 
-Example: isAdult(21) should return 'Adult'.
+Example: isCharAVowel('a') should return true.
 
 Complete the exercise in the space below:
 */
+// Let me try ternary expression
+const isCharAVowel = (char) =>
+  char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
+    ? "Vowel"
+    : "Not A Vowel";
 
-const isAdult = (x) => {
-  const adultAge = 18;
-  if (x >= adultAge) {
-    return "Adult";
-  } else {
-    return "Minor";
-  }
-};
-
-console.log("Exercise 2 Result:", isAdult(21));
-// Expect result to be Adult
+console.log("Exercise 3 Result:", isCharAVowel("a"));
+// Expected result would be vowel
