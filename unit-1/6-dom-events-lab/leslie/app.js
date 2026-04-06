57c624531f093de5d43c3591300c01eff88b21ba
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-06 22:28:56 +0800

    Update app.js - refactor code

--- unit-1/6-dom-events-lab/leslie/app.js
+++ unit-1/6-dom-events-lab/leslie/app.js
@@ -5,31 +5,72 @@ const cachedActions = [];
 
 /*------------------------ Cached Element References ------------------------*/
 const display = document.querySelector(".display");
+const multipicativeStr = "*/";
 
 /*----------------------------- Event Listeners -----------------------------*/
 // EventListener to listen for clicks within #calculator parent
 document.querySelector("#calculator").addEventListener("click", function (e) {
   // console.log(e.target.innerHTML);
+
   // Check if the target's class has the prefix of "button"
   if (e.target.classList.contains("button")) {
-    switch (e.target.innerText) {
-      case "C":
-        cachedActions.length = 0;
-        break;
-      case "=":
-        resolveActions();
-        break;
-      default:
-        // add new action object
-        cachedActions.push({
-          isNum: e.target.className === "button number",
-          value: e.target.innerText,
-        });
-        console.log(cachedActions);
-        break;
-    }
+    const splitedClassName = e.target.className.split(" ");
 
-    updateDisplay();
+    if (splitedClassName.length > 1) {
+      const buttonType = splitedClassName[1];
+      const buttonValue = e.target.innerText;
+      // console.log(buttonType, buttonValue);
+
+      switch (buttonType) {
+        case "operator":
+          if (buttonValue === "C") {
+            // clear cached actions
+            clearActions();
+          } else {
+            // check last action in cache
+            const lastCachedAction = cachedActions.at(-1);
+            // console.log(lastCachedAction);
+
+            if (lastCachedAction.type === "operator") {
+              if (lastCachedAction.value !== buttonValue) {
+                if (buttonValue === "-") {
+                  // if last action was a multiplicative operation treat the - (minus)
+                  // as a negative sign
+                  if (multipicativeStr.includes(lastCachedAction.value)) {
+                    addAction("number", buttonValue);
+                  } else {
+                    lastCachedAction.value = buttonValue;
+                  }
+                } else {
+                  lastCachedAction.value = buttonValue;
+                }
+              }
+            } else {
+              // if last action type was a number, check if the value is a negative sign
+              if (lastCachedAction.value === "-") {
+                // remove the negative sign and the operator
+                // before it before adding new operator
+                cachedActions.splice(-2, 2);
+              }
+              addAction(buttonType, buttonValue);
+            }
+          }
+          break;
+        case "number":
+          if (cachedActions.length === 1 && cachedActions[0].value === "0") {
+            cachedActions[0].value = buttonValue;
+          } else {
+            addAction(buttonType, buttonValue);
+          }
+          break;
+        case "equals":
+          break;
+        default:
+          break;
+      }
+
+      updateDisplay();
+    }
   }
 });
 
@@ -40,15 +81,44 @@ function updateDisplay() {
   display.innerHTML = valueArr.join("");
 }
 
-function resolveActions() {
-  const formattedActions = ["0"];
-  let lastActionIsNumber = true;
-  for (const actionObj of cachedActions) {
-    if (actionObj.isNum) {
-      if (lastActionIsNumber) {
-        formattedActions[formattedActions.length - 1] += actionObj.value;
-      }
-    }
-  }
-  console.log(formattedActions);
+function addAction(type, value) {
+  cachedActions.push({
+    type: type,
+    value: value,
+  });
+  console.log(cachedActions);
 }
+
+function clearActions() {
+  cachedActions.length = 0;
+  addAction("number", "0");
+}
+
+// function resolveActions() {
+//   const formattedActions = ["0"];
+//   let lastActionIsNumber = true;
+
+//   for (const actionObj of cachedActions) {
+//     if (lastActionIsNumber) {
+//       if (actionObj.isNum) {
+//         // concat actionObj.value to the value of the last action
+//         formattedActions[formattedActions.length - 1] += actionObj.value;
+//       } else {
+//         formattedActions.push(actionObj.value);
+//         lastActionIsNumber = false;
+//       }
+//     } else {
+//       // last action is an operator
+//       if (actionObj.isNum) {
+//         formattedActions.push(actionObj.value);
+//         lastActionIsNumber = true;
+//       } else {
+//         //
+//       }
+//     }
+//   }
+//   console.log(formattedActions);
+// }
+
+clearActions();
+updateDisplay();
