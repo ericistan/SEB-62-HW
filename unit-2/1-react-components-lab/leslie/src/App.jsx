03ce159fac53877de40ae055b0b63ae7b2b9b75c
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-21 16:01:13 +0800

    Update App.jsx with mapping logic to display data using WeatherForecast component

--- unit-2/1-react-components-lab/leslie/src/App.jsx
+++ unit-2/1-react-components-lab/leslie/src/App.jsx
@@ -1,10 +1,61 @@
 import React from "react";
+import WeatherForecast from "./WeatherForecast";
+
+const weatherForecasts = [
+  {
+    day: "Mon",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg",
+    imgAlt: "sun icon",
+    conditions: "sunny",
+    time: "Morning",
+  },
+  {
+    day: "Tue",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg",
+    imgAlt: "moon icon",
+    conditions: "clear",
+    time: "Night",
+  },
+  {
+    day: "Wed",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg",
+    imgAlt: "clouds with lightning icon",
+    conditions: "stormy",
+    time: "All Day",
+  },
+  {
+    day: "Thu",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg",
+    imgAlt: "sun overcast by clouds icon",
+    conditions: "overcast",
+    time: "Evening",
+  },
+  {
+    day: "Fri",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg",
+    imgAlt: "moon overcast by clouds icon",
+    conditions: "cloudy",
+    time: "Night",
+  },
+];
 
 function App() {
+  const weatherData = weatherForecasts.map((item, idx) => {
+    return (
+      <WeatherForecast
+        day={item.day}
+        img={item.img}
+        imgAlt={item.imgAlt}
+        conditions={item.conditions}
+        time={item.time}
+      />
+    );
+  });
   return (
-    <div>
-      <h2>GA SEB</h2>
-    </div>
+    <>
+      <h1 className="centered">Local Weather</h1>
+      <section>{weatherData}</section>
+    </>
   );
 }
 
