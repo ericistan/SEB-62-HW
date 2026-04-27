c96778765f18dc291109e15c827afe0466f8421e
ET <eric.tanms@gmail.com>
2026-04-27 22:33:14 +0800

    complete homework

--- unit-2/4-lifting-state-in-react-lab/Eric/lifting-state-in-react-lab/src/App.jsx
+++ unit-2/4-lifting-state-in-react-lab/Eric/lifting-state-in-react-lab/src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+// src/App.jsx
+
 import "./App.css";
+import BurgerStack from "./components/BurgerStack";
+import IngredientList from "./components/IngredientList";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [stack, setStack] = useState([]);
+  const availableIngredients = [
+    { name: "Kaiser Bun", color: "saddlebrown" },
+    { name: "Sesame Bun", color: "sandybrown" },
+    { name: "Gluten Free Bun", color: "peru" },
+    { name: "Lettuce Wrap", color: "olivedrab" },
+    { name: "Beef Patty", color: "#3F250B" },
+    { name: "Soy Patty", color: "#3F250B" },
+    { name: "Black Bean Patty", color: "#3F250B" },
+    { name: "Chicken Patty", color: "burlywood" },
+    { name: "Lettuce", color: "lawngreen" },
+    { name: "Tomato", color: "tomato" },
+    { name: "Bacon", color: "maroon" },
+    { name: "Onion", color: "lightyellow" },
+    { name: "Cheddar Cheese", color: "#FDE18B" },
+    { name: "Swiss Cheese", color: "#F1E1A8" },
+  ];
 
-  return <></>;
-}
+  function addToBurger(ingredient) {
+    setStack([...stack, ingredient]);
+  }
+
+  function removeFromBurger(index) {
+    setStack(stack.filter((_, i) => i !== index));
+  }
+
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+        <IngredientList
+          ingredients={availableIngredients}
+          addToBurger={addToBurger}
+        />
+        <BurgerStack ingredients={stack} removeFromBurger={removeFromBurger} />
+      </section>
+    </main>
+  );
+};
 
 export default App;
