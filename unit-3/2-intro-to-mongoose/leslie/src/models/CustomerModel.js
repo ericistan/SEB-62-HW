a10d5c25d4df23a1f200de84f3708e336ca0f743
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-13 15:58:23 +0800

    Move folder to src

--- unit-3/2-intro-to-mongoose/leslie/src/models/CustomerModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const CustomerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-});
-
-export default mongoose.model("Customer", CustomerSchema);
