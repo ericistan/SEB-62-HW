37b7ecfaa6265997cddefa8156cdcc8336fea93d
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-15 19:32:08 +0800

    Refactor validator

--- unit-3/4-men-stack-relating-data-cookbook/leslie/A-Embedding/src/validators/userValidator.js
+++ unit-3/4-men-stack-relating-data-cookbook/leslie/A-Embedding/src/validators/userValidator.js
@@ -3,23 +3,23 @@ import { body, param } from "express-validator";
 export const checkGetAllFoodsInputs = [param("userId").exists().withMessage("userId is required")];
 
 export const checkCreateFoodsInputs = [
-  param("userId").exists().withMessage("userId is required"),
+  param("userId").isMongoId().withMessage("userId is required"),
   body("foods").exists().withMessage("foods is required").isArray().withMessage("foods must be an array"),
   body("foods.*.name").notEmpty().withMessage("name of food cannot be empty"),
 ];
 
 export const checkGetFoodByUserIdAndFoodId = [
-  param("userId").exists().withMessage("userId is required"),
-  param("foodId").exists().withMessage("foodId is required"),
+  param("userId").isMongoId().withMessage("userId is required"),
+  param("foodId").isMongoId().withMessage("foodId is required"),
 ];
 
 export const checkUpdateFoodByUserIdAndFoodId = [
-  param("userId").exists().withMessage("userId is required"),
-  param("foodId").exists().withMessage("foodId is required"),
+  param("userId").isMongoId().withMessage("userId is required"),
+  param("foodId").isMongoId().withMessage("foodId is required"),
   body("name").notEmpty().withMessage("name of food cannot be empty"),
 ];
 
 export const checkDeleteFoodByUserIdAndFoodId = [
-  param("userId").exists().withMessage("userId is required"),
-  param("foodId").exists().withMessage("foodId is required"),
+  param("userId").isMongoId().withMessage("userId is required"),
+  param("foodId").isMongoId().withMessage("foodId is required"),
 ];
