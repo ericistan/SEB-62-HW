c66154554c301ccef55ac8dfe0fc4b1965af72a6
leslietan1981 <leslietan.yw1981@gmail.com>
2026-06-03 23:57:04 +0800

    Complete exercise 3

--- unit-4/3-python-data-structures/leslie/exercises.py
+++ unit-4/3-python-data-structures/leslie/exercises.py
@@ -9,7 +9,8 @@ def manage_students():
     students = ["Kenneth", "Eric", "Weng Soon", "Lincoln"]
     first_student = students[0]
     last_student = students[-1]
-    return f"Students: {students}. First student: {first_student}. Last student: {last_student}"
+    # return f"Students: {students}. First student: {first_student}. Last student: {last_student}"
+    return students, first_student, last_student
 
 # Call the function and print the result
 print('Exercise 1:', manage_students())
@@ -31,7 +32,21 @@ def combine_foods():
 
     meal = meal[:-2]
 
-    return f"meal = {meal}."
+    return meal, foods
 
 # Call the function and print the result
 print('Exercise 2:', combine_foods())
+
+
+# Exercise 3: Slicing Tuples
+#
+# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods(foods):
+    # your code here
+    last_two_foods = foods[-2:]
+    return last_two_foods
+
+# Call the function and print the result
+meal, foods = combine_foods()
+print('Exercise 3:', slice_foods(foods))
