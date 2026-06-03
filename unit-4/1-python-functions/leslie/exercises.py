ef7b78396768424e2afa60aad6779b0604c6dbb1
leslietan1981 <leslietan.yw1981@gmail.com>
2026-06-03 16:38:17 +0800

    Complete exercise 9

--- unit-4/1-python-functions/leslie/exercises.py
+++ unit-4/1-python-functions/leslie/exercises.py
@@ -152,3 +152,36 @@ def product(*args):
 
 print('Exercise 8:', product(-1, 4))
 print('Exercise 8:', product(2, 5, 5))
+
+
+# Exercise 9: Basic Calculator
+#
+# Create a function named `basic_calculator` that takes three arguments:
+# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide').
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important,
+# treat the first parameter as the first operand and the second parameter as the second operand.
+#
+# Examples:
+# basic_calculator(10, 5, 'subtract') should return 5.
+# basic_calculator(10, 5, 'add') should return 15.
+# basic_calculator(10, 5, 'multiply') should return 50.
+# basic_calculator(10, 5, 'divide') should return 2.
+#
+# Define the function and then call it below.
+
+def basic_calculator(a, b, operation):
+    match operation:
+        case "add":
+            return a + b
+        case "subtract":
+            return a - b
+        case "multiply":
+            return a * b
+        case "divide":
+            return a / b
+    return None
+
+print('Exercise 9 Result:', basic_calculator(10, 5, "subtract"))
+print('Exercise 9 Result:', basic_calculator(10, 5, "add"))
+print('Exercise 9 Result:', basic_calculator(10, 5, "multiply"))
+print('Exercise 9 Result:', basic_calculator(10, 5, "divide"))
