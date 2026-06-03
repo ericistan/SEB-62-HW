73614e5404103e01d8f24d407eb2e97128b3f509
leslietan1981 <leslietan.yw1981@gmail.com>
2026-06-03 17:35:05 +0800

    Complete exercise 3

--- unit-4/2-python-control-flow/leslie/exercises.py
+++ unit-4/2-python-control-flow/leslie/exercises.py
@@ -57,11 +57,46 @@ def check_voting_eligibility():
     voting_age = 18
     while True:
         try:
-            user_age = input("Enter your age: ")
-            print("You are eligible to vote." if int(user_age) >= voting_age else "You are not eligible to vote.")
+            user_age = int(input("Enter your age: "))
+            print("You are eligible to vote." if user_age >= voting_age else "You are not eligible to vote.")
             return
         except ValueError as error:
             print("Invalid input try again")
 
 # Call the function
 check_voting_eligibility()
+
+
+# Exercise 3: Calculate Dog Years
+#
+# Write a Python function named `calculate_dog_years` that calculates a dog's age in dog years.
+# Fill in the logic to perform the calculation inside the function.
+#
+# Function Details:
+# - Prompt the user to enter a dog's age: "Input a dog's age: "
+# - Calculate the dog's age in dog years:
+#      - The first two years of the dog's life count as 10 dog years each.
+#      - Each subsequent year counts as 7 dog years.
+# - Print the calculated age: "The dog's age in dog years is xx."
+# - Replace 'xx' with the calculated dog years.
+#
+# Hints:
+# - Use the `input()` function to capture user input.
+# - Convert the string input to an integer using `int()`.
+# - Apply conditional logic to perform the correct age calculation based on the dog's age.
+
+def calculate_dog_years():
+    # Your control flow logic goes here
+    while True:
+        try:
+            dog_age = int(input("Input a dog's age: "))
+            dog_years = 0
+            for year in range(dog_age):
+                dog_years = dog_years + 10 if year < 2 else dog_years + 7
+            print(f"The dog's age in dog years is {dog_years}")
+            return
+        except ValueError as error:
+            print("Invalid input try again")
+
+# Call the function
+calculate_dog_years()
